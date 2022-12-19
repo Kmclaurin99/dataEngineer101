0355c51c469b0984c8972b4c14cc433f304eec22
Kwan McLaurin <Kmclaurin99@yahoo.com>
2022-12-19 16:58:00 -0500

    firt commit is name generator

---
+++ bandName.py
@@ -0,0 +1,7 @@
+print("Hello!")
+print ("What is the name of the city you grew up in?")
+user_city = input ()
+print ("What is the name of your pet?")
+user_pet_name = input()
+print ("Computer generated band name:")
+print(user_city +" "+ user_pet_name)
